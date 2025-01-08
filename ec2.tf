8a90e34dd6951a35eefb1ca16eb1179b00464c0c
Bhargavi Bairagoni <113205911+bhargavibairagoni@users.noreply.github.com>
2025-01-08 19:48:06 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,55 +1,58 @@
-#provider "aws" {
-#region = var.region
-#}
 resource "aws_security_group" "demosg" {
+  name        = "demosg"
+  description = "Demo security group"
+  vpc_id      = var.vpc_id
 
-ingress{
-from_port=80
-to_port=80
-protocol="tcp"
-cidr_blocks=["0.0.0.0/0"]
-}
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-ingress{
-from_port=3306
-to_port=3306
-protocol="tcp"
-cidr_blocks=["0.0.0.0/0"]
-}
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-ingress{
-from_port=22
-to_port=22
-protocol="tcp"
-cidr_blocks=["0.0.0.0/0"]
-}
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-ingress{
-from_port=8080
-to_port=8080
-protocol="tcp"
-cidr_blocks=["0.0.0.0/0"]
-}
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-egress{
-from_port=0
-to_port=0
-protocol="-1"
-cidr_blocks=["0.0.0.0/0"]
-}
-tags={
-Name="WEB SG"
-}
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "WEB SG"
+  }
 }
 
 resource "aws_instance" "public_subnet-1" {
-ami="ami-06744fbd0847bf4f5"
-instance_type="t2.micro"
-count=1
-key_name="balaji"
-vpc_security_group_ids=["${aws_security_group.demosg.id}"]
-user_data="${file("userdata.sh")}"
-tags={
-Name="POLL Scm"
-}
+  ami               = "ami-06744fbd0847bf4f5"
+  instance_type     = "t2.micro"
+  count             = 1
+  key_name          = "balaji"
+  vpc_security_group_ids = [aws_security_group.demosg.id]
+  subnet_id         = var.subnet_id
+  user_data         = file("userdata.sh")
+
+  tags = {
+    Name = "POLL Scm"
+  }
 }
